90bf0e8589698f10999323fc1d4b04a3c8806dcc
Muhammad Saad <saad.skynners@gmail.com>
2020-04-27 23:56:18 +0500

    Create Permission1.sol

---
+++ NodeApis/Contracts/Permission1.sol
@@ -0,0 +1,23 @@
+pragma solidity >=0.4.22 <0.6.0;
+//pragma experimental ABIEncoderV2;
+contract Permission {
+   
+    struct ProvidersHavingAccessToPatient {
+        string providerName;
+        int32 accessLevel;
+    }
+    
+    struct PatientsAccessibleByProvider {
+        string patientCNIC;
+        int32 accessLevel;
+    }
+   
+    mapping (string => ProvidersHavingAccessToPatient[]) ProvidersHavingAccessToPatientsList;
+    mapping (string => PatientsAccessibleByProvider[]) PatientsAccessibleByProvidersList;
+ 
+    address owner;
+   
+    constructor() public {
+        owner = msg.sender;
+    }
+}
